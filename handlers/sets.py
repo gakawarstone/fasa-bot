6e331938a78956d69b20148ca36a067ab5d44f87
Gakawarstone <pinnekerv@gmail.com>
2022-03-08 14:45:22 +0600

    feat: add sets operations

--- handlers/sets.py
+++ handlers/sets.py
@@ -1,19 +1,47 @@
+from multiprocessing.connection import answer_challenge
 import aiogram
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
 from bot_config import bot
 
+data = {}
+
 
 async def hello(message: aiogram.types.Message):
     await message.answer('Приветсвую вас этот бот выполняет операции над множествами')
-    bot.add_state_handler(FSM.get_data, get_data)
-    await FSM.get_data.set()
+    await message.answer('Введите множество А через запятую [1, 2, 3]')
+    bot.add_state_handler(FSM.get_set_a, get_set_a)
+    await FSM.get_set_a.set()
 
 
-async def get_data(message: aiogram.types.Message, state: FSMContext):
+async def get_set_a(message: aiogram.types.Message, state: FSMContext):
     await state.finish()
+    data['set_a'] = set(message.text.split(', '))
+    await message.answer('Введите множество B через запятую [1, 2, 3]')
+    bot.add_state_handler(FSM.get_set_b, get_set_b)
+    await FSM.get_set_b.set()
+
+
+async def get_set_b(message: aiogram.types.Message, state: FSMContext):
+    await state.finish()
+    data['set_b'] = set(message.text.split(', '))
+    await message.answer('Если хотите получить ответ то напишите да')
+    bot.add_state_handler(FSM.print_result, print_result)
+    await FSM.print_result.set()
+
+
+async def print_result(message: aiogram.types.Message, state: FSMContext):
+    await state.finish()
+    a = data['set_a']
+    b = data['set_b']
+    await message.answer('Пересечение = ' + str(a.intersection(b)))
+    await message.answer('Обьединение = ' + str(a.union(b)))
+    await message.answer('Разность А В = ' + str(a - b))
+    await message.answer('Разность В А = ' + str(b - a))
 
 
 class FSM(StatesGroup):
-    get_data = State()
+    get_set_a = State()
+    get_set_b = State()
+    print_result = State()
